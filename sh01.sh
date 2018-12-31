3a855126b87c835b0f13fc9bcd2b270bac4d899e
xiaoming719 <2532275320@qq.com>
2018-12-31 17:49:17 +0800

    xiaoming commit sh01.sh

---
+++ sh01.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Program
+#    This program shows "Hello World!" in yours screen
+#History 
+#Author xiaoming Time 2018/12/31 Version 1.1.1
+echo "Hello World! \n"
+exit 'Success print'
